cbfd27c4ff86c3811a120baf5e9be4b121779e2f
ranelya <149768902+ranelya@users.noreply.github.com>
2024-03-04 15:38:07 +0600

    added section questions

---
+++ js/core.js
@@ -0,0 +1,16 @@
+const accordions = document.querySelectorAll('.accordion')
+
+accordions.forEach((accordion) => {
+  accordion.addEventListener('click', () => {
+    // Закрываем все аккордионы перед открытием текущего
+    accordions.forEach((otherAccordion) => {
+      if (otherAccordion !== accordion) {
+        otherAccordion.querySelector('.accordion-body').classList.remove('active')
+      }
+    })
+
+    // Открываем или закрываем текущий аккордион
+    const body = accordion.querySelector('.accordion-body')
+    body.classList.toggle('active')
+  })
+})
